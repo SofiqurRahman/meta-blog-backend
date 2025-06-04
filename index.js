90f3cb9869bff6071af3e53d736e1830efb3596a
Sofiqur <sofiqurr849@gmail.com>
2025-06-04 08:06:23 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// routes
+const blogRoutes = require("./src/routes/blog.routes");
+app.use("/blogs", blogRoutes);
+
+// mongoose configuration
+async function main() {
+  await mongoose.connect(process.env.DB_URL);
+}
+
+main()
+  .then(() => console.log("Mongodb connected Successfully!"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Meta Blog App Sever is running...!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ src/routes/blog.routes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const {
+  getAllBlogs,
+  getBlogById,
+  postANewBlog,
+  deleteABlogById,
+  updateABlogById,
+} = require("../controllers/blog.controllers");
+
+const router = express.Router();
+
+// get all blogs
+router.get("/", getAllBlogs);
+
+// get a single blog by id
+router.get("/:id", getBlogById);
+
+// post a new blog
+router.post("/add-post", postANewBlog);
+
+// delete a blog
+router.delete("/:id", deleteABlogById);
+
+// update a blog
+router.put("/:id", updateABlogById);
+
+module.exports = router;
