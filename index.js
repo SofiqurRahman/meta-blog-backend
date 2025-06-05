5a56eead3ee6a9cf5fb1efb6e51d363b334a19c9
Sofiqur <sofiqurr849@gmail.com>
2025-06-05 02:27:19 -0400

    first commit

--- index.js
+++ index.js
@@ -16,15 +16,15 @@ app.use("/blogs", blogRoutes);
 // mongoose configuration
 async function main() {
   await mongoose.connect(process.env.DB_URL);
+
+  app.get("/", (req, res) => {
+    res.send("Meta Blog App Sever is running...!");
+  });
 }
 
 main()
   .then(() => console.log("Mongodb connected Successfully!"))
   .catch(err => console.log(err));
-
-app.get("/", (req, res) => {
-  res.send("Meta Blog App Sever is running...!");
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
